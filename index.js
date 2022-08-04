19da36e81108a6362f20161673fe320bb811debe
Snowsore <snow_sore@hotmail.com>
2022-08-04 13:55:14 +0800

    Web server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const fallback = require("express-history-api-fallback");
+
+const root = __dirname + "/public";
+
+app.use(express.static(root));
+app.use(fallback("index.html", { root: root }));
+
+app.listen("8080", () => {
+  console.log("Server start at 8080");
+});
